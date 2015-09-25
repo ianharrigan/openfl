f857fa2bf65c12a328d1416b16950e44f5f8001e
Ian Harrigan <ianharrigan@hotmail.com>
2015-09-25 20:05:59 +0200

    Basic primitives (before refactor)

--- openfl/_internal/renderer/dom/DOMElements.hx
+++ openfl/_internal/renderer/dom/DOMElements.hx
@@ -1,17 +1,30 @@
 package openfl._internal.renderer.dom;
 
-import haxe.io.Bytes;
 import js.Browser;
-import lime.graphics.Image;
-import lime.graphics.utils.ImageCanvasUtil;
-import lime.utils.UInt8Array;
-import openfl.display.Bitmap;
+import js.html.Element;
 import openfl.display.BitmapData;
 import openfl.display.DisplayObject;
 import openfl.display.GradientType;
 import openfl.display.Stage;
-import js.html.Element;
 
+typedef ColorOptions = {
+	@:optional var thickness:Null<Float>;
+	@:optional var color:Null<Int>;
+	@:optional var alpha:Null<Float>;
+}
+
+typedef GradientOptions = {
+	@:optional var type:GradientType;
+	@:optional var colors:Array<Dynamic>;
+	@:optional var alphas:Array<Dynamic>;
+	@:optional var ratios:Array<Dynamic>;
+}
+
+typedef BitmapOptions = {
+	@:optional var base64:String;
+	@:optional var width:Null<Int>;
+	@:optional var height:Null<Int>;
+}
 
 typedef StrokeOptions = {
 	@:optional var lineThickness:Null<Float>;
@@ -22,6 +35,25 @@ typedef StrokeOptions = {
 	@:optional var lineGradientColors:Array<Dynamic>;
 	@:optional var lineGradientAlphas:Array<Dynamic>;
 	@:optional var lineGradientRatios:Array<Dynamic>;
+	
+	@:optional var lineBitmapBase64:String;
+	@:optional var lineBitmapWidth:Null<Int>;
+	@:optional var lineBitmapHeight:Null<Int>;
+
+}
+
+typedef FillOptions = {
+	@:optional var fillColor:Null<Int>;
+	@:optional var fillAlpha:Null<Float>;
+	
+	@:optional var fillGradientType:GradientType;
+	@:optional var fillGradientColors:Array<Dynamic>;
+	@:optional var fillGradientAlphas:Array<Dynamic>;
+	@:optional var fillGradientRatios:Array<Dynamic>;
+	
+	@:optional var fillBitmapBase64:String;
+	@:optional var fillBitmapWidth:Null<Int>;
+	@:optional var fillBitmapHeight:Null<Int>;
 }
 
 @:access(openfl.display.DisplayObject)
@@ -76,7 +108,7 @@ class DOMElements {
 		}
 	}
 	
-	public static inline function renderBorder(el:Element, x:Float, y:Float, cx:Float, cy:Float, strokeOptions:StrokeOptions) {
+	public static inline function renderBorder(primitive:String, el:Element, x:Float, y:Float, cx:Float, cy:Float, strokeOptions:StrokeOptions) {
 		if (strokeOptions.lineThickness != null) {
 			cx += strokeOptions.lineThickness;
 			cy += strokeOptions.lineThickness;
@@ -86,23 +118,167 @@ class DOMElements {
 		el.style.width = cx + "px";
 		el.style.height = cy + "px";
 		
+		var t:Float = 0;
 		if (strokeOptions.lineThickness != null) {
+			t = strokeOptions.lineThickness * 0.5;
 			//sub.style.boxShadow = "inset 0px 0px 0px " + lineThickness + "px " + color(lineColor, lineAlpha);
-			el.style.border = strokeOptions.lineThickness + "px solid " + color(strokeOptions.lineColor, strokeOptions.lineAlpha);
-			var t = strokeOptions.lineThickness * 0.5;
-			el.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -" + t + ", -" + t + ", 0, 1)";
-			el.style.borderRadius = t + "px";
+			if (strokeOptions.lineGradientType == null && strokeOptions.lineBitmapBase64 == null) {
+				el.style.border = strokeOptions.lineThickness + "px solid " + color(strokeOptions.lineColor, strokeOptions.lineAlpha);
+			}
+			
+			if (primitive == "circle") {
+				t = cx * 0.5;
+			} else if (primitive == "ellipse") {
+				el.style.borderRadius = "50%";
+			}
 		}
 		
-		if (strokeOptions.lineGradientType != null) {
-			el.style.borderImage = "linear-gradient(to bottom, "
-				+ color(strokeOptions.lineGradientColors[0]) + " 0%, "
-				+ color(strokeOptions.lineGradientColors[1]) + " 100%)";
-			el.style.borderImageSlice = "1";
+		if (strokeOptions.lineGradientType != null || strokeOptions.lineBitmapBase64 != null) {
+			if (primitive == "rectangle") {
+				var l:Float = strokeOptions.lineThickness; // 6
+				var cx2:Float = cx - l;
+				var cy2:Float = cy - l;
+				el.style.setProperty("-webkit-clip-path", 'polygon(0 0,' 
+																+ '0 ${cy}px,'
+																+ '${l}px ${cy}px,'
+																+ '${l}px ${l}px,'
+																+ '${cx2}px ${l}px,'
+																+ '${cx2}px ${cy2}px,'
+																+ '0 ${cy2}px,'
+																+ '0 ${cy}px,'
+																+ '${cx}px ${cy}px,'
+																+ '${cx}px 0)', null);
+			
+				if (strokeOptions.lineGradientType != null) {
+					el.style.backgroundImage = "linear-gradient(to left, "
+						+ color(strokeOptions.lineGradientColors[1]) + " -150%, "
+						+ color(strokeOptions.lineGradientColors[0]) + " 250%)";
+					//el.style.borderImageSlice = "1";
+				} else if (strokeOptions.lineBitmapBase64 != null) {
+					el.style.backgroundImage = "url(" + strokeOptions.lineBitmapBase64 + ")";
+					el.style.backgroundPosition = '${t}px ${t}px';
+				}
+			} else if (primitive == "circle" || primitive == "ellipse") { // gradient/image circles are more complex so delegate to svg - probably not used that much
+				var svg:Element = Browser.document.createElementNS("http://www.w3.org/2000/svg", "svg");
+				var defs:Element = Browser.document.createElementNS("http://www.w3.org/2000/svg", "defs");
+				svg.appendChild(defs);
+				el.appendChild(svg);
+				
+				var strokeId:String = "id" + Std.random(0xFFFFFF);
+				if (primitive == "circle") {
+					var circle:Element = Browser.document.createElementNS("http://www.w3.org/2000/svg", "circle");
+					circle.setAttribute("cx", "" + (cx * 0.5));
+					circle.setAttribute("cy", "" + (cy * 0.5));
+					circle.setAttribute("r", "" + ((cx - strokeOptions.lineThickness) * 0.5));
+					circle.setAttribute("stroke-width", "" + strokeOptions.lineThickness);
+					circle.setAttribute("stroke", "url(#" + strokeId + ")");
+					circle.setAttribute("fill", "transparent");
+					svg.appendChild(circle);
+				} else if (primitive == "ellipse") {
+					var ellipse:Element = Browser.document.createElementNS("http://www.w3.org/2000/svg", "ellipse");
+					ellipse.setAttribute("cx", "" + (cx - strokeOptions.lineThickness));
+					ellipse.setAttribute("cy", "" + (cy - strokeOptions.lineThickness));
+					ellipse.setAttribute("rx", "" + ((cx - strokeOptions.lineThickness) / 2));
+					ellipse.setAttribute("ry", "" + ((cy - strokeOptions.lineThickness) / 2));
+					ellipse.setAttribute("stroke-width", "" + strokeOptions.lineThickness);
+					ellipse.setAttribute("stroke", "url(#" + strokeId + ")");
+					ellipse.setAttribute("fill", "transparent");
+					svg.appendChild(ellipse);
+				}
+				
+				if (strokeOptions.lineGradientType != null) {
+					var linearGradient:Element = Browser.document.createElementNS("http://www.w3.org/2000/svg", "linearGradient");
+					defs.appendChild(linearGradient);
+					linearGradient.setAttribute("id", strokeId);
+					linearGradient.setAttribute("x1", "-150%");
+					linearGradient.setAttribute("y1", "0%");
+					linearGradient.setAttribute("x2", "200%");
+					linearGradient.setAttribute("y2", "0%");
+					
+					var stop:Element = Browser.document.createElementNS("http://www.w3.org/2000/svg", "stop");
+					stop.setAttribute("offset", "-150%");
+					stop.setAttribute("stop-color", color(strokeOptions.lineGradientColors[0]));
+					linearGradient.appendChild(stop);
+					
+					var stop:Element = Browser.document.createElementNS("http://www.w3.org/2000/svg", "stop");
+					stop.setAttribute("offset", "200%");
+					stop.setAttribute("stop-color", color(strokeOptions.lineGradientColors[1]));
+					linearGradient.appendChild(stop);
+				} else if (strokeOptions.lineBitmapBase64 != null) {
+					var pattern:Element = Browser.document.createElementNS("http://www.w3.org/2000/svg", "pattern");
+					defs.appendChild(pattern);
+					pattern.setAttribute("id", strokeId);
+					pattern.setAttribute("patternUnits", "userSpaceOnUse");
+					pattern.setAttribute("x", "-" + (strokeOptions.lineThickness * 0.5));
+					pattern.setAttribute("y", "-" + (strokeOptions.lineThickness * 0.5));
+					pattern.setAttribute("height", "" + strokeOptions.lineBitmapWidth);
+					pattern.setAttribute("width", "" + strokeOptions.lineBitmapHeight);
+					
+					var image:Element = Browser.document.createElementNS("http://www.w3.org/2000/svg", "image");
+					image.setAttribute("height", "" + strokeOptions.lineBitmapWidth);
+					image.setAttribute("width", "" + strokeOptions.lineBitmapHeight);
+					image.setAttributeNS("http://www.w3.org/1999/xlink", "href", strokeOptions.lineBitmapBase64);
+					pattern.appendChild(image);
+				}
+			}
+		}
+		
+		
+		if (t > 0) {
+			el.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -" + t + ", -" + t + ", 0, 1)";
+			if (primitive != "ellipse") {
+				el.style.borderRadius = t + "px";
+			} else if (strokeOptions.lineGradientType != null || strokeOptions.lineBitmapBase64 != null) {
+				el.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -" + Math.ffloor((cx - strokeOptions.lineThickness) * 0.5) + ", -" + Math.ffloor((cy - strokeOptions.lineThickness) * 0.5) + ", 0, 1)";
+			}
+		}
+	}
+	
+	public static inline function renderFill(primitive:String, el:Element, parent:Element, x:Float, y:Float, cx:Float, cy:Float, fillOptions:FillOptions, strokeOptions:StrokeOptions) {
+		if (fillOptions.fillColor != null) {
+			if (fillOptions.fillGradientType == null) {
+				el.style.backgroundColor = color(fillOptions.fillColor, fillOptions.fillAlpha);
+			}
 		}
 
+		if (strokeOptions.lineGradientType != null || strokeOptions.lineBitmapBase64 != null) {
+			var fill:Element = Browser.document.createElement("div");
+			fill.style.left = x + "px";
+			fill.style.top = y + "px";
+			fill.style.width = cx + "px";
+			fill.style.height = cy + "px";
+			fill.classList.add("shape");
+			
+			parent.appendChild(fill);
+			el = fill;
+		}
+		
+		var t:Float = 0;
+		if (fillOptions.fillGradientType != null || fillOptions.fillBitmapBase64 != null) {
+			if (fillOptions.fillGradientType != null) {
+				el.style.backgroundImage = "linear-gradient(to left, "
+					+ color(fillOptions.fillGradientColors[1]) + " -150%, "
+					+ color(fillOptions.fillGradientColors[0]) + " 250%)";
+			} else if (fillOptions.fillBitmapBase64 != null) {
+				el.style.backgroundImage = "url(" + fillOptions.fillBitmapBase64 + ")";
+				el.style.backgroundPosition = '${t}px ${t}px';
+			}
+		}
+		
+			
+		if (primitive == "circle") {
+			t = cx * 0.5;
+			el.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -" + t + ", -" + t + ", 0, 1)";
+			
+			el.style.borderRadius = (cx * 0.5) + "px";
+		} else if (primitive == "ellipse") {
+			//t = cx * 0.5;
+			el.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -" + t + ", -" + t + ", 0, 1)";
+			
+			el.style.borderRadius = "50%";
+		}
+		
 	}
-											 
 	
 	public static inline function renderGraphics(shape:DisplayObject):Void {
 		var el:Element = map.get(shape);
@@ -116,19 +292,9 @@ class DOMElements {
 		
 		var data = new DrawCommandReader(shape.__graphics.__commands);
 		var sub:Element = null;
-		
-		/*
-		var lineThickness:Null<Float> = null;
-		var lineColor:Null<Int> = null;
-		var lineAlpha:Null<Float> = null;
-		
-		var lineGradientType:GradientType = null;
-		var lineGradientColors:Array<Dynamic> = null;
-		var lineGradientAlphas:Array<Dynamic> = null;
-		var lineGradientRatios:Array<Dynamic> = null;
-		*/
-		
-		var strokeOptions:StrokeOptions = {};
+		var strokeOptions:StrokeOptions = { };
+		var fillOptions:FillOptions = {};
+		var primitive:String = null;
 		
 		for (type in shape.__graphics.__commands.types) {
 			switch (type) {
@@ -137,13 +303,6 @@ class DOMElements {
 					strokeOptions.lineThickness = c.thickness;
 					strokeOptions.lineColor = c.color;
 					strokeOptions.lineAlpha = c.alpha;
-					/*
-					strokeOptions = {
-						lineThickness = c.thickness;
-						lineColor = c.color;
-						lineAlpha = c.alpha;
-					};
-					*/
 				
 				case LINE_GRADIENT_STYLE:	
 					var c = data.readLineGradientStyle();
@@ -153,36 +312,32 @@ class DOMElements {
 					strokeOptions.lineGradientRatios = c.ratios;
 					
 				case LINE_BITMAP_STYLE:
-					var c = data.readLineBitmapStyle ();
-					//strokeCommands.lineBitmapStyle (c.bitmap, c.matrix, c.repeat, c.smooth);
-					/*
-					trace(c.bitmap);
-					trace(ImageCanvasUtil.convertToData(c.bitmap.image));
-					trace(c.bitmap);
-					*/
+					var c = data.readLineBitmapStyle();
+					strokeOptions.lineBitmapBase64 = getImageBase64(c.bitmap);
+					strokeOptions.lineBitmapWidth = c.bitmap.width;
+					strokeOptions.lineBitmapHeight = c.bitmap.height;
 					
-					var x = createImageBase64(c.bitmap);
-					var image = Browser.document.createImageElement();
-					image.src = x;
-					Browser.document.body.appendChild(image);
-					image.style.position = "absolute";
-					image.style.left = "0";
-					image.style.top = "0";
-					trace(x);
-					
-				case BEGIN_FILL:
 				case DRAW_RECT:
-					sub = Browser.document.createElement("div"); 
-					sub.classList.add("shape");
+					if (sub == null) {
+						sub = Browser.document.createElement("div"); 
+						sub.classList.add("shape");
+					}
 
 					var c = data.readDrawRect();
-					renderBorder(sub, c.x, c.y, c.width, c.height, strokeOptions);
+					primitive = "rectangle";
+					renderBorder(primitive, sub, c.x, c.y, c.width, c.height, strokeOptions);
+					renderFill(primitive, sub, el, c.x, c.y, c.width, c.height, fillOptions, strokeOptions);
+
 				case DRAW_ROUND_RECT:	
-					sub = Browser.document.createElement("div"); 
-					sub.classList.add("shape");
+					if (sub == null) {
+						sub = Browser.document.createElement("div"); 
+						sub.classList.add("shape");
+					}
 
 					var c = data.readDrawRoundRect();
-					renderBorder(sub, c.x, c.y, c.width, c.height, strokeOptions);
+					primitive = "rectangle";
+					renderBorder(primitive, sub, c.x, c.y, c.width, c.height, strokeOptions);
+					renderFill(primitive, sub, el, c.x, c.y, c.width, c.height, fillOptions, strokeOptions);
 					
 					if (c.rx == c.ry) {
 						sub.style.borderRadius = Math.fceil(c.rx * 0.5) + "px";
@@ -197,9 +352,50 @@ class DOMElements {
 						sub.style.borderBottomRightRadius = s;
 					}
 					
+				case DRAW_CIRCLE:	
+					if (sub == null) {
+						sub = Browser.document.createElement("div"); 
+						sub.classList.add("shape");
+					}
+
+					var c = data.readDrawCircle();
+					primitive = "circle";
+					renderBorder(primitive, sub, c.x, c.y, c.radius * 2, c.radius * 2, strokeOptions);
+					renderFill(primitive, sub, el, c.x, c.y, c.radius * 2, c.radius * 2, fillOptions, strokeOptions);
+				
+				case DRAW_ELLIPSE:	
+					if (sub == null) {
+						sub = Browser.document.createElement("div"); 
+						sub.classList.add("shape");
+					}
+					
+					var c = data.readDrawEllipse();
+					primitive = "ellipse";
+					renderBorder(primitive, sub, c.x, c.y, c.width, c.height, strokeOptions);
+					renderFill(primitive, sub, el, c.x, c.y, c.width, c.height, fillOptions, strokeOptions);
+					
+				case BEGIN_FILL:
+					var c = data.readBeginFill();
+					fillOptions.fillColor = c.color;
+					fillOptions.fillAlpha = c.alpha;
+					
+				case BEGIN_GRADIENT_FILL:
+					var c = data.readLineGradientStyle();
+					fillOptions.fillGradientType = c.type;
+					fillOptions.fillGradientColors = c.colors;
+					fillOptions.fillGradientAlphas = c.alphas;
+					fillOptions.fillGradientRatios = c.ratios;
+					
+				case BEGIN_BITMAP_FILL:	
+					var c = data.readLineBitmapStyle();
+					fillOptions.fillBitmapBase64 = getImageBase64(c.bitmap);
+					fillOptions.fillBitmapWidth = c.bitmap.width;
+					fillOptions.fillBitmapHeight = c.bitmap.height;
+					
 				case END_FILL:
 					if (sub != null) {
 						el.appendChild(sub);
+						sub = null;
 					}
 
 				default:
@@ -208,30 +404,8 @@ class DOMElements {
 		}
 	}
 	
-	private static function createImageBase64(b:BitmapData):String {
-		//trace(b);
-		//return "";
-		var b = b.clone(); // probably doesnt make sense, or to be creating images at all
-		ImageCanvasUtil.convertToData(b.image);
-		trace(b);
-		//var ba:UInt8Array = new UInt8Array();
-		//ba = b.image.buffer.data;
-		trace(b.image.buffer.data.buffer);
-		trace(b.image.buffer.__srcImageData.data.buffer);
-		trace(Image.fromBytes(b.image.buffer.data.buffer));
-		
-		/*
-		for (i in 0...b.image.buffer.data.buffer.byteLength) {
-			trace(b.image.buffer.data.);
-		}
-		*/
-		
-		var bytes:Bytes = Bytes.ofData(b.image.buffer.data.buffer);
-		//var bytes:Bytes = b.image.buffer.__srcImageData.data.buffer.toBytes();
-		trace(bytes.length);
-		var base64:String = haxe.crypto.Base64.encode(bytes);
-		var imgSrc:String = "data:image/png;base64," + base64;
-		return imgSrc;
+	private static function getImageBase64(b:BitmapData):String {
+		return b.image.buffer.__srcCanvas.toDataURL();
 	}
 	
 	public static inline function isComplexGraphics(shape:DisplayObject):Bool {
